61d97c8e6cb6fccca0803229360ea59f9dbf274a
Andrew Mayne <andrew.c.mayne@gmail.com>
2013-02-17 11:53:04 +1100

    added env variable fix

---
+++ config/app_environment_variables-example.rb
@@ -0,0 +1,6 @@
+ENV['GMAIL_USERNAME']="test@example.com"
+ENV['GMAIL_PASSWORD']="pwd"
+ENV['ADMIN_NAME']="ADMIN"
+ENV['ADMIN_EMAIL']="admin@admin.com"
+ENV['ADMIN_PASSWORD']="admin123"
+ENV['FACEBOOK_APPID']="XXXXXXXXXX"
